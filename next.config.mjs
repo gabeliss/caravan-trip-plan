fd8c0c6583dbe888bd6be72415af6ec5f5e854ac
Gabriel Liss <gabeliss@umich.edu>
2025-03-17 20:57:48 -0500

    Fix image domains configuration for Vercel deployment

--- next.config.mjs
+++ next.config.mjs
@@ -8,6 +8,33 @@ const nextConfig = {
     // Also ignore TypeScript errors for the production build
     ignoreBuildErrors: true,
   },
+  images: {
+    domains: [
+      'images.unsplash.com',
+      'caravan-bucket.s3.us-east-2.amazonaws.com',
+      'i.imgur.com',
+      'images.pexels.com'
+    ],
+    // Alternatively, you can use remotePatterns for more flexibility
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: '**.unsplash.com',
+      },
+      {
+        protocol: 'https',
+        hostname: '**.amazonaws.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'i.imgur.com',
+      },
+      {
+        protocol: 'https',
+        hostname: '**.pexels.com',
+      }
+    ]
+  }
 };
 
 export default nextConfig; 
